613467dc68af7a3458eee2e4b12ec934150fb906
Thomas Klinger <Thomas.Klinger@otto.de>
2017-02-18 18:59:11 +0100

    Add getter and setter to PreparedGraphiteQuery

--- src/main/java/de/teekaay/graphite/PreparedGraphiteQuery.java
+++ src/main/java/de/teekaay/graphite/PreparedGraphiteQuery.java
@@ -10,4 +10,20 @@ public class PreparedGraphiteQuery {
     public String render() {
         return query;
     }
+
+    public String getQuery() {
+        return query;
+    }
+
+    public PreparedGraphiteQuery setQuery(final String query) {
+        this.query = query;
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return "PreparedGraphiteQuery{" +
+                "query='" + query + '\'' +
+                '}';
+    }
 }
